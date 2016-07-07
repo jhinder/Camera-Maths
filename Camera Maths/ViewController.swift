af2c5ebffd63190b388d08ad44b2a7feebf7ec67
Jan Hindermann <jan@dfragment.net>
2016-07-07 19:52:09 +0200

    Initial Commit

---
+++ Camera Maths/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera Maths
+//
+//  Created by Jan on 07.07.16.
+//  Copyright © 2016 dfragment.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
